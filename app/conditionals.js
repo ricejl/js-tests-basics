ce74440ba1689633ed3c54b85252053d7a678d1c
Jenny <ricejl@gmail.com>
2019-11-12 17:01:51 -0700

    finished through switches

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,12 +1,16 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
-
+  //   if (num1 > num2) {
+  //     return num1;
+  //   } else if (num1 < num2) {
+  //     return num2;
+  //   }
+  return Math.max(num1, num2);
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
-// Example: 
+// Example:
 // input: 23, 25
 // output: "A"
 // Example:
@@ -14,12 +18,19 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+  let percentage = Math.round((score / possible) * 100);
+  if (percentage >= 90) {
+    return "A";
+  } else if (percentage >= 80) {
+    return "B";
+  } else if (percentage >= 70) {
+    return "C";
+  } else if (percentage >= 60) {
+    return "D";
+  } else return "F";
 }
 
-
 // --------------------------------------------
-
 
 // 3. Given an integer that represents an hour in the day (1 - 24), write a function that returns "good morning", "good afternoon", "good evening", or "good night" based off of what hour it is. You'll need to think in military time.
 //Morning is between 5am and 11am (5 - 11)
@@ -29,34 +40,43 @@ function testGrader(score, possible) {
 //Make sure your ranges are inclusive
 
 function timeOfDayGreeting(hour) {
-    // you code below
-
+  if (hour >= 22 || hour <= 4) {
+    return "good night";
+  } else if (hour >= 18) {
+    return "good evening";
+  } else if (hour >= 12) {
+    return "good afternoon";
+  } else return "good morning";
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+  if (temp >= 103) {
+    return "fever go to hospital";
+  } else if (temp > 98.6) {
+    return "fever";
+  } else return "no fever";
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
 let myCar = {
-    make: "Ford",
-    model: "Mustang",
-    color: "Red",
-    moving: false
-}
+  make: "Ford",
+  model: "Mustang",
+  color: "Red",
+  moving: false
+};
 
 function isStopped(car) {
-
+  return car.moving != true;
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
 
 let dish = {
-    yourDish: true,
-    isDirty: true
-}
+  yourDish: true,
+  isDirty: true
+};
 
 function washDish(dish) {
-
+  return dish.yourDish && dish.isDirty;
 }
--- app/switches.js
+++ app/switches.js
@@ -1,20 +1,65 @@
-// 1. Given a string with the value of a day of the week, return the number that that day is in the week. A second argument will be provided to determine if should start week on Monday if true, else Sunday if false. If the string is not a day of the week but is bad input, then return the string 'That's not a day of the week'.
-// Example: 
-    // input: 'Sunday', false
-    // output: 1
+// 1. Given a string with the value of a day of the week, return the number that that day is in the week. A second argument will be provided to determine if true, start the week on Monday, else Sunday is the start of the week. If the string is not a day of the week but is bad input, then return the string 'That's not a day of the week'.
+// Example:
+// input: 'Sunday', false
+// output: 1
 //Example:
-    // input: 'Sunday', true
-    // output: 7
+// input: 'Sunday', true
+// output: 7
 
 function daysPosition(day, offset) {
-
+  day = day.toLowerCase();
+  //if offset is true what do i wanna do? can handle mathematically
+  switch (day) {
+    case "monday":
+      if (offset) {
+        return 1;
+      }
+      return 2;
+      break;
+    case "tuesday":
+      if (offset) {
+        return 2;
+      }
+      return 3;
+      break;
+    case "wednesday":
+      if (offset) {
+        return 3;
+      }
+      return 4;
+      break;
+    case "thursday":
+      if (offset) {
+        return 4;
+      }
+      return 5;
+      break;
+    case "friday":
+      if (offset) {
+        return 5;
+      }
+      return 6;
+      break;
+    case "saturday":
+      if (offset) {
+        return 6;
+      }
+      return 7;
+      break;
+    case "sunday":
+      if (offset) {
+        return 7;
+      }
+      return 1;
+      break;
+    default:
+      return "That's not a day of the week";
+  }
 }
-
 
 // --------------------------------------------
 
-
-/** 2. Given a both a score and a score for par, return the corresponding term:
+/** 2. Given both a score and a score for par, return the corresponding term:
  *           difference |   term
  *    -----------------------------------
  *  (score == 1) || -3  |   "Ace"
@@ -26,31 +71,70 @@ function daysPosition(day, offset) {
  *            >= +3     |   "Ouch"
  */
 
- function golfScore(score, par) {
-
- }
- 
+function golfScore(score, par) {
+  let correctedScore = score - par;
+  switch (correctedScore) {
+    case -3:
+      return "Ace";
+      break;
+    case -2:
+      return "Eagle";
+      break;
+    case -1:
+      return "Birdie";
+      break;
+    case 0:
+      return "Par";
+      break;
+    case 1:
+      return "Bogie";
+      break;
+    case 1:
+      return "Double Bogie";
+      break;
+    default:
+      return "Ouch";
+      break;
+  }
+}
 
 // --------------------------------------------
 
-
-// 3. Counting Cards. In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called card counting.  
+// 3. Counting Cards. In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called card counting.
 // Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
 /**
  *      Value   |   Cards
  *   --------------------------------
  *       +1     |   '2', '3', '4', '5', '6'
- *        0     |	'7', '8', '9' 
- *       -1	    |   '10', 'J', 'Q', 'K','A' 
+ *        0     |	'7', '8', '9'
+ *       -1	    |   '10', 'J', 'Q', 'K','A'
  */
 // Write a card counting function that will receive a card. The function will increment or decrement the global count variable according to the card's value (see table above). The function will then return the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative.
 // Example:
-    // output: '-5 Hold'
+// output: '-5 Hold'
 // Example:
-    // output: '2 Bet'
+// output: '2 Bet'
 
-let count = 0
+let count = 0;
 
 function cardCounter(card) {
+  switch (card) {
+    case "2" || "3" || "4" || "5" || "6":
+      count++;
+      break;
+    case "7" || "8" || "9":
+      count;
+      break;
+    default:
+      count--;
+  }
 
+  let currentCount = count;
+  switch (currentCount) {
+    case currentCount > 0:
+      return currentCount + " Bet";
+      break;
+    default:
+      return currentCount + " Hold";
+  }
 }
